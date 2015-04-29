abe8aeaa85809969d897fa6752902f8f3e18b5a0
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2015-04-29 21:09:56 +0100

    fix blog urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.BlogIndex.as_view(), name="index"),
-    url(r'^entry/add/$', views.BlogAddEntry.as_view(), name="entry-new"),
-    url(r'^entry/edit/$', views.BlogEditEntry.as_view(), name="entry-edit"),
+    url(r'^add/$', views.BlogAddEntry.as_view(), name="entry-new"),
+    url(r'^edite/(?P<pk>[\d]+)/$', views.BlogEditEntry.as_view(), name="entry-edit"),
     url(r'^entry/(?P<slug>\S+)$', views.BlogDetail.as_view(), name="entry-detail"),
     url(r'^login/$', views.LoginView.as_view(), name='login'),
     url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
