99bea712beafdc918acc2add286d58611e15364c
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2015-04-29 17:58:51 +0100

    change scaffold urls

--- scaffold/urls.py
+++ scaffold/urls.py
@@ -9,11 +9,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'scaffold.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^blog/', include('blog.urls')),
     url(r'^_ah/', include('djangae.urls')),
-
     # Note that by default this is also locked down with login:admin in app.yaml
     url(r'^admin/', include(admin.site.urls)),
-
     url(r'^csp/', include('cspreports.urls')),
 )
